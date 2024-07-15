fab838c3c7e25163e424d4a82f0361432166b417
IZGI-A <iremzehraaltun@gmail.com>
2024-07-15 18:00:01 +0300

    create dashboard skeleton

--- finance-client/src/components/FlexContainer.tsx
+++ finance-client/src/components/FlexContainer.tsx
@@ -1,10 +1,11 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
-const FlexContainer = styled(Box) ({
+const FlexContainer = styled(Box) (({ theme }) => ({
+  backgroundColor: theme.palette.background.dark,
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-});
+}));
 
 export default FlexContainer;
